7d2e8787ea6a48f1130a655a317ac2274d4ad6b8
omarhernandezrey <hernandezreyomar@gmail.com>
2023-07-08 16:46:20 -0500

    03-Elementos de un Lenguaje de Programación: Variables, Funciones y Sintaxis___

--- 01_valores.js
+++ 01_valores.js
@@ -1,12 +1,23 @@
 40
+// Tipo: number
 
 "Omar Hernandez Rey"
+// Tipo: string
 
 true
+// Tipo: boolean
+
 false
+// Tipo: boolean
 
-null 
+null
+// Tipo: object
+
 undefined
+// Tipo: undefined
 
-[1,2,3]
+[1, 2, 3]
+// Tipo: object (array)
+
 {nombre: "Omar Hernandez"}
+// Tipo: object
