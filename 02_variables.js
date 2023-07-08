a3e66aecaed124dc6a7e3649e9a7c20c761a4421
omarhernandezrey <hernandezreyomar@gmail.com>
2023-07-08 17:08:30 -0500

    04 Qué es una variable en JavaScript

---
+++ 02_variables.js
@@ -0,0 +1,26 @@
+var nombre = "Omar";
+// Se declara una variable llamada "nombre" y se le asigna el valor "Omar".
+
+// Declarar variable
+var edad;
+
+edad
+// La variable "edad" ha sido declarada pero aún no se le ha asignado un valor, por lo que devuelve "undefined".
+
+// Inicializar variable
+edad = 30;
+
+edad
+// Ahora la variable "edad" ha sido inicializada con el valor 30 y lo muestra.
+
+var elementos = ["Computadora", "Celular"];
+// Se declara una variable llamada "elementos" y se le asigna un array con dos elementos: "Computadora" y "Celular".
+
+var persona = {
+    nombre: "Omar",
+    edad: 32
+};
+// Se declara una variable llamada "persona" y se le asigna un objeto con dos propiedades: "nombre" y "edad".
+
+persona;
+// Retorna el objeto completo {nombre: "Omar", edad: 32}.
