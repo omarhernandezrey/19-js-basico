909b1f94de2f5be701e5dd08fa7696ddf8b133d8
omarhernandezrey <hernandezreyomar@gmail.com>
2023-07-19 13:44:14 -0500

    18 Switch

---
+++ 10_switch.js
@@ -0,0 +1,19 @@
+var numero = 1;
+
+switch (numero) {
+    case 1:
+        console.log("Soy uno!"); // Imprime "Soy uno!" en la consola
+        break; // La instrucción "break" indica que se debe salir del switch después de ejecutar este caso.
+
+    case 10:
+        console.log("Soy diez!"); // No se ejecuta en este ejemplo
+        break;
+
+    case 100:
+        console.log("Soy cien!"); // No se ejecuta en este ejemplo
+        break;
+
+    default:
+        console.log("No soy nada!"); // No se ejecuta en este ejemplo
+        break;
+}
